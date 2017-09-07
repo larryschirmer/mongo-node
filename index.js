d1d50b2d26e0350b6a0f43434060c144e376201b
larryschirmer <larryschirmer@gmail.com>
2017-09-07 11:40:34 -0400

    add updateDoc

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // docker run --name mongo-docker -it -p 2000:27017 -d mongo
 // docker exec -t -i mongo-docker /bin/bash
 
-let { getDB, insertDoc } = require('./ioFunctions');
+let { getDB, insertDoc, getDoc, updateDoc } = require('./ioFunctions');
 let { onerror } = require('./errorFunctions');
 
 let MongoClient = require('mongodb').MongoClient,
@@ -13,7 +13,10 @@ let dbURL = 'mongodb://0.0.0.0:2000/database';
 
 co(function* () {
   let db = yield getDB(dbURL);
-  let response = yield insertDoc(db);
-  console.log(response);
+  // let insertResponse = yield insertDoc(db);
+  // console.log(insertResponse);
+  getDoc(db);
+  // let updateResponse = yield updateDoc(db);
+  // console.log(updateResponse);
   db.close();
 }).catch(onerror);
--- ioFunctions.js
+++ ioFunctions.js
@@ -1,19 +1,21 @@
 var MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
 
+let { onerror } = require('./errorFunctions');
+
 module.exports.getDB = addr => {
-        // Connection URL
-        var url = addr;
-      
-        return new Promise((res, rej) => {
-          // Use connect method to connect to the server
-          MongoClient.connect(url, function (err, db) {
+    // Connection URL
+    var url = addr;
+
+    return new Promise((res, rej) => {
+        // Use connect method to connect to the server
+        MongoClient.connect(url, function (err, db) {
             assert.equal(null, err);
             console.log('Connected successfully to server');
             res(db);
-          })
         })
-      }
+    }).catch(onerror);
+}
 
 var schema = {
     key: 'value',
@@ -23,8 +25,30 @@ module.exports.insertDoc = db => {
     return new Promise((res, rej) => {
         db.collection('docs').insertOne(schema, (err, result) => {
             assert.equal(err, null);
-            console.log("Inserted a document into the restaurants collection.");
+            console.log("Inserted a document into the docs collection.");
             res('done');
         });
-    })
+    }).catch(onerror);
+};
+
+module.exports.getDoc = db => {
+    return new Promise((res, rej) => {
+        let cursor = db.collection('docs').find();
+        cursor.forEach(entry => {
+            let output = JSON.stringify(entry, null, 4);
+            console.log(output);
+        })
+    }).catch(onerror);
+};
+
+module.exports.updateDoc = db => {
+    return new Promise((res, rej) => {
+        let cursor = db.collection('docs').updateOne(
+            { "key": "value (New)" },
+            {
+                $set: { "key": "value" }
+            }, (err, result) => {
+                res('value changed');
+            })
+    }).catch(onerror);
 };
