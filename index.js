382046c73165ff6ac279f7f165b7d8554b19a85f
larryschirmer <larryschirmer@gmail.com>
2017-09-07 10:54:30 -0400

    async and modularize io functions

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 // docker run --name mongo-docker -it -p 2000:27017 -d mongo
 // docker exec -t -i mongo-docker /bin/bash
 
-let { insertDoc } = require('./ioFunctions');
-// let { onerror } = require('./errorFunctions');
+let { getDB, insertDoc } = require('./ioFunctions');
+let { onerror } = require('./errorFunctions');
 
 let MongoClient = require('mongodb').MongoClient,
   assert = require('assert'),
   co = require('co');
 
-// Connection URL
-var url = 'mongodb://0.0.0.0:2000/pathfinding';
 
-// Use connect method to connect to the server
-MongoClient.connect(url, function (err, db) {
-  assert.equal(null, err);
-  console.log('Connected successfully to server');
-  insertDoc(db).then(console.log);
+let dbURL = 'mongodb://0.0.0.0:2000/database';
+
+co(function* () {
+  let db = yield getDB(dbURL);
+  let response = yield insertDoc(db);
+  console.log(response);
   db.close();
-});
-
-// co(function* () {
-
-// }).catch(onerror);
+}).catch(onerror);
--- ioFunctions.js
+++ ioFunctions.js
@@ -1,6 +1,20 @@
 var MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
 
+module.exports.getDB = addr => {
+        // Connection URL
+        var url = addr;
+      
+        return new Promise((res, rej) => {
+          // Use connect method to connect to the server
+          MongoClient.connect(url, function (err, db) {
+            assert.equal(null, err);
+            console.log('Connected successfully to server');
+            res(db);
+          })
+        })
+      }
+
 var schema = {
     key: 'value',
 };
