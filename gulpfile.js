a737e1653b0655a61ad5838efcd8a20e0cac3d71
larryschirmer <larryschirmer@gmail.com>
2017-09-08 09:48:07 -0400

    add complete build automation commands

--- gulpfile.js
+++ gulpfile.js
@@ -17,8 +17,47 @@ gulp.task(
     'stop-mongo-docker-db',
     shell.task([
         'echo ',
-        'echo Stopping Mongo Docker',
+        'echo Stopping Mongo Docker Database',
         'docker container stop mongo-docker-db',
         'docker container rm mongo-docker-db'
+    ])
+);
+
+gulp.task(
+    'start-mongo-docker-db',
+    shell.task([
+        'echo ',
+        'echo Starting Mongo Docker Database',
+        'docker run --name mongo-docker-db -it --net=mongo-docker-net -d mongo',
+    ])
+);
+
+gulp.task(
+    'stop-mongo-docker-api',
+    shell.task([
+        'echo ',
+        'echo Stopping Mongo Docker API',
+        'docker container stop mongo-docker-api',
+        'docker container rm mongo-docker-api',
+    ])
+);
+
+gulp.task(
+    'start-mongo-docker-api',
+    shell.task([
+        'echo ',
+        'echo Starting Mongo Docker API',
+        'docker build -t larryschirmer/mongo-docker .',
+        'docker run --name mongo-docker-api -p 1234:8080 --net=mongo-docker-net -d larryschirmer/mongo-docker'
+    ])
+);
+
+gulp.task(
+    'start-mongo-docker',
+    shell.task([
+        'echo ',
+        'echo Starting Mongo Docker',
+        'gulp start-mongo-docker-db',
+        'gulp start-mongo-docker-api',
     ])
 );
